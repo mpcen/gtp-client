207a380aa601a79f9e22268aaa479ae924294c66
Manny Martinez <emmanuelmartinez.dev@gmail.com>
2020-10-13 23:47:12 -0700

    kb avoiding view on signup

--- src/auth/SignupScreen.tsx
+++ src/auth/SignupScreen.tsx
@@ -5,6 +5,8 @@ import {
     StyleSheet,
     SafeAreaView,
     TouchableOpacity,
+    KeyboardAvoidingView,
+    Platform,
 } from 'react-native';
 import { Input, Button, Text, Icon } from 'react-native-elements';
 import { useNavigation } from '@react-navigation/native';
@@ -30,100 +32,106 @@ export const SignupScreen = () => {
 
     return (
         <SafeAreaView style={styles.container}>
-            <View style={styles.backButtonContainer}>
-                <TouchableOpacity onPress={() => navigate('AuthHome')}>
-                    <Icon
-                        iconStyle={styles.iconStyle}
-                        name='chevron-left'
-                        size={40}
-                    />
-                </TouchableOpacity>
-            </View>
-
-            <View style={styles.titleContainer}>
-                <Text style={styles.title} allowFontScaling={false}>
-                    {Constants.SignupTitle}
-                </Text>
-            </View>
-
-            <View style={styles.formContainer}>
-                {/* EMAIL */}
-                <Input
-                    testID='input-email'
-                    placeholder={Constants.EmailPlaceholder}
-                    disabled={isLoading}
-                    value={email}
-                    onChangeText={setEmail}
-                />
-
-                {/* PASSWORD */}
-                <View style={styles.passwordContainer}>
-                    <Input
-                        testID='input-password'
-                        disabled={isLoading}
-                        secureTextEntry={!isPasswordVisible}
-                        placeholder={Constants.PasswordPlaceholder}
-                        value={password}
-                        onChangeText={setPassword}
-                    />
-                    <PasswordVisibilityIcon
-                        isPasswordVisible={isPasswordVisible}
-                        setIsPasswordVisible={setIsPasswordVisible}
-                    />
+            <KeyboardAvoidingView
+                behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+                style={styles.kbAvoidingView}
+            >
+                <View style={styles.backButtonContainer}>
+                    <TouchableOpacity onPress={() => navigate('AuthHome')}>
+                        <Icon
+                            iconStyle={styles.iconStyle}
+                            name='chevron-left'
+                            size={40}
+                        />
+                    </TouchableOpacity>
                 </View>
 
-                {/* CONFIRM PASSWORD */}
-                <View style={styles.passwordContainer}>
-                    <Input
-                        testID='input-confirm-password'
-                        disabled={isLoading}
-                        secureTextEntry={!isPasswordVisible}
-                        placeholder={Constants.PasswordConfirmedPlaceholder}
-                        value={confirmedPassword}
-                        onChangeText={setConfirmedPassword}
-                        errorMessage={error.length ? error : ''}
-                        errorStyle={styles.errorMessageContainer}
-                    />
-                    <PasswordVisibilityIcon
-                        isPasswordVisible={isPasswordVisible}
-                        setIsPasswordVisible={setIsPasswordVisible}
-                    />
+                {/* TITLE */}
+                <View style={styles.titleContainer}>
+                    <Text style={styles.title} allowFontScaling={false}>
+                        {Constants.SignupTitle}
+                    </Text>
                 </View>
 
-                {/* BUTTON */}
-                <Button
-                    containerStyle={styles.buttonContainer}
-                    testID='button-sign-up'
-                    title={Constants.SignUpButtonText}
-                    disabled={isLoading}
-                    onPress={() =>
-                        dispatch(signUp(email, password, confirmedPassword))
-                    }
-                />
-            </View>
+                <View style={styles.formContainer}>
+                    {/* EMAIL */}
+                    <Input
+                        testID='input-email'
+                        placeholder={Constants.EmailPlaceholder}
+                        disabled={isLoading}
+                        value={email}
+                        onChangeText={setEmail}
+                    />
 
-            <View style={styles.loadingContainer}>
-                {isLoading && <ActivityIndicator testID='indicator-loading' />}
-            </View>
+                    {/* PASSWORD */}
+                    <View style={styles.passwordContainer}>
+                        <Input
+                            testID='input-password'
+                            textContentType="oneTimeCode"
+                            disabled={isLoading}
+                            secureTextEntry={!isPasswordVisible}
+                            placeholder={Constants.PasswordPlaceholder}
+                            value={password}
+                            onChangeText={setPassword}
+                        />
+                        <PasswordVisibilityIcon
+                            isPasswordVisible={isPasswordVisible}
+                            setIsPasswordVisible={setIsPasswordVisible}
+                        />
+                    </View>
+
+                    {/* CONFIRM PASSWORD */}
+                    <View style={styles.passwordContainer}>
+                        <Input
+                            testID='input-confirm-password'
+                            textContentType="oneTimeCode"
+                            disabled={isLoading}
+                            secureTextEntry={!isPasswordVisible}
+                            placeholder={Constants.PasswordConfirmedPlaceholder}
+                            value={confirmedPassword}
+                            errorMessage={error.length ? error : ''}
+                            errorStyle={styles.errorMessageContainer}
+                            onChangeText={setConfirmedPassword}
+                        />
+                        <PasswordVisibilityIcon
+                            isPasswordVisible={isPasswordVisible}
+                            setIsPasswordVisible={setIsPasswordVisible}
+                        />
+                    </View>
+
+                    {/* BUTTON */}
+                    <Button
+                        containerStyle={styles.buttonContainer}
+                        testID='button-sign-up'
+                        title={Constants.SignUpButtonText}
+                        disabled={isLoading}
+                        onPress={() =>
+                            dispatch(signUp(email, password, confirmedPassword))
+                        }
+                    />
+
+                    <View style={styles.loadingContainer}>
+                        {isLoading && <ActivityIndicator testID='indicator-loading' />}
+                    </View>
+                </View>
+            </KeyboardAvoidingView>
         </SafeAreaView>
     );
 };
 
 const styles = StyleSheet.create({
-    container: { flex: 1, alignItems: 'center' },
+    container: { flex: 1 },
+    kbAvoidingView: { flex: 1 },
     backButtonContainer: {
-        marginTop: 40,
-        width: '100%',
-        justifyContent: 'flex-start',
-        alignContent: 'flex-start',
+        paddingTop: 20,
+        marginLeft: 4
     },
     iconStyle: { alignSelf: 'flex-start' },
-    titleContainer: {
-        flex: 1,
+    titleContainer: { 
+        flex: 0.25,       
         width: '100%',
-        marginLeft: 40,
-        justifyContent: 'center',
-        alignContent: 'flex-start',
+        paddingLeft: 16,
+        justifyContent: 'flex-end'
     },
     title: {
         fontSize: 32,
@@ -131,8 +139,10 @@ const styles = StyleSheet.create({
     formContainer: {
         flex: 1,
         width: '100%',
-        paddingLeft: 12,
-        paddingRight: 12,
+        paddingLeft: 8,
+        paddingRight: 8,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
     },
     passwordContainer: { flexDirection: 'row', position: 'relative' },
     passwordVisibilityButton: {
@@ -142,7 +152,7 @@ const styles = StyleSheet.create({
         top: 8,
         justifyContent: 'center',
     },
-    buttonContainer: { marginTop: 24, width: '33%', alignSelf: 'center' },
+    buttonContainer: { paddingTop: 24, width: '33%', alignSelf: 'center' },
     loadingContainer: { flex: 0.5, justifyContent: 'center' },
     errorMessageContainer: { textAlign: 'center', paddingTop: 4 },
 });
