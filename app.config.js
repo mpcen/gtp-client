a5f52e28466e191a2d7154c24218a357359313b0
manny martinez <vccengineers12@gmail.com>
2020-10-23 00:00:34 -0700

    config files

--- app.config.js
+++ app.config.js
@@ -1,15 +1,22 @@
 import 'dotenv/config';
 
 export default {
-    extra: {
-        API_URI: process.env.API_URI,
-    },
-    android: {
-        package: 'com.garbagetimeapp.client',
-    },
     name: 'GARBAGETIME.APP',
     slug: 'garbagetime-app',
     icon: './assets/icon.png',
     version: '0.0.1',
     orientation: 'portrait',
+    extra: {
+        API_URI: process.env.API_URI,
+    },
+    android: {
+        package: 'com.garbagetime_app',
+        versionCode: 1,
+        permissions: [],
+    },
+    ios: {
+        bundleIdentifier: 'com.garbagetime-app',
+        buildNumber: '0.0.1',
+        supportsTablet: true,
+    },
 };
