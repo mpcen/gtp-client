af29564e77ab0b2bb37d1c5f9e51ab6abdfcee83
manny martinez <vccengineers12@gmail.com>
2020-10-29 21:07:50 -0700

    update

--- app.config.js
+++ app.config.js
@@ -2,21 +2,32 @@ import 'dotenv/config';
 
 export default {
     name: 'GARBAGETIME.APP',
+    description: 'GARBAGETIME.APP',
     slug: 'garbagetime-app',
     icon: './assets/icon.png',
-    version: '0.0.4',
+    version: '0.0.5',
+    updates: {
+        fallbackToCacheTimeout: 0,
+    },
+    assetBundlePatterns: ['**/*'],
     orientation: 'portrait',
     extra: {
         API_URI: process.env.API_URI,
     },
     android: {
         package: 'com.garbagetime_app',
-        versionCode: 4,
+        versionCode: 5,
         permissions: [],
+        splash: {
+            backgroundColor: '#000000',
+        },
     },
     ios: {
         bundleIdentifier: 'com.garbagetime-app',
-        buildNumber: '0.0.4',
+        buildNumber: '0.0.5',
         supportsTablet: true,
+        splash: {
+            backgroundColor: '#000000',
+        },
     },
 };
